8c834e44ff57db2d8180dfa703381b0c8bd17ee9
Ziyahid <ziyahid27@gmail.com>
2025-07-07 11:01:29 +0530

    Switched to PostgreSQL for Render hosting

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ using StudentRegistrationAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Register DbContext
+// New (PostgreSQL)
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
